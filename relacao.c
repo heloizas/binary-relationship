b1f1ee8b528f102ec336f0db674f30c31ec6ba63
Heloiza Santos <heloizasantos@live.com>
2020-08-23 11:15:47 -0300

    Working reflexive and irreflexive

--- relacao.c
+++ relacao.c
@@ -6,13 +6,10 @@
 
 void printmatrix(int M[][MAXTAM], int n, int m, int name) {
 	int i, j;
-	printf("n: %d, m: %d \n", n, m);
-	printf("size: %d \n", name);
-
 	printf("\n");
     //i e j igual a 1 para não imprimir coluna e linha com índice 0
 	for(i=name; i<n; i++) {
-		for(j=name; j<m; j++){ 
+		for(j=name; j<m; j++) { 
 			printf("%d ", M[i][j]);
 		}
 		printf("\n");
@@ -21,7 +18,8 @@ void printmatrix(int M[][MAXTAM], int n, int m, int name) {
 }
 
 int main() {
-    int size, name, maxname, n, m, i, j, first, second, contadorref = 0, M[MAXTAM][MAXTAM];
+    int size, name, maxname, n, m, i, j, first, second, 
+        contadorref = 0, contadorirref = 0, M[MAXTAM][MAXTAM];
     char linha[MAXTAM];
     char reflexiva = 'F';
     char irreflexiva = 'F';
@@ -49,7 +47,6 @@ int main() {
 
     while(fgets(linha, 100, file) != NULL) {
         fscanf(file, "%i %i", &first, &second);
-        // printf("%i %i\n", first, second);
         M[first][second] = 1;
     }
 
@@ -57,26 +54,44 @@ int main() {
 
     //Verificação Reflexiva
     for(i=name; i<n; i++) {
-        if(M[i][i] == 1){
+        if(M[i][i] == 1) {
             contadorref += 1;
         }
+        else {
+            // printf("Para ser reflexiva: (%d,%d);\n", i,i);
+        }
     }
-    if (contadorref == size){
+    if (contadorref == size) {
         reflexiva = 'V';
     }
     
+    //Verificação Irreflexiva
+     for(i=name; i<n; i++) {
+        if(M[i][i] == 0) {
+            contadorirref += 1;
+        }
+        else {
+            // printf("Para ser Irreflexiva: (%d,%d);\n", i,i);
+        }
+    }
+    if (contadorirref == size) {
+        irreflexiva = 'V';
+    }
+
+    //Verificação Simétrica
+    
     // Verificação das relações encontradas
-    for(i=0; i<n; i++) {
-		for(j=0; j<m; j++) {
-			if(M[i][j] == 1){
-                printf("Tem relação: %i %i\n", i, j);
-            }
-		}
-	}
+    // for(i=0; i<n; i++) {
+	// 	for(j=0; j<m; j++) {
+	// 		if(M[i][j] == 1){
+    //             printf("Tem relação: %i %i\n", i, j);
+    //         }
+	// 	}
+	// }
 
     //Saída
     saida = fopen("saida.txt", "w");
-    fprintf(saida, "Reflexiva: %c\nIrreflexiva:\nSimétrica:\nAnti-simétrica:\nAssimétrica:\nTransitiva:\nRelação de equivalência:\nRelação de ordem parcial:\nFecho transitivo da relação: %d", reflexiva, size);
+    fprintf(saida, "Reflexiva: %c\nIrreflexiva: %c\nSimétrica:\nAnti-simétrica:\nAssimétrica:\nTransitiva:\nRelação de equivalência:\nRelação de ordem parcial:\nFecho transitivo da relação: %d", reflexiva, irreflexiva, size);
 
     fclose(file);
     fclose(saida);
