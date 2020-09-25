3f375fc6dfa0420a4e1650f86449862a0ca3391a
Heloiza Santos <heloiza@MacBook-Pro-de-Heloiza.local>
2020-09-24 22:00:44 -0300

    Done Transitive

--- relacao.c
+++ relacao.c
@@ -11,13 +11,13 @@ typedef struct
     int y;
 } Tupla;
 
-void printmatrix(int M[][MAXTAM], int n, int m, int name) {
+void printmatrix(int Matriz[][MAXTAM], int n, int m, int name) {
 	int i, j;
 	printf("\n");
     //i e j igual a 1 para não imprimir coluna e linha com índice 0
 	for(i=name; i<n; i++) {
 		for(j=name; j<m; j++) { 
-			printf("%d ", M[i][j]);
+			printf("%d ", Matriz[i][j]);
 		}
 		printf("\n");
 	}
@@ -28,14 +28,14 @@ int main() {
     int size, name, maxname, n, m, i, j, first, second, 
         contadorref = 0, contadorirref = 0, contadorsimetrica = 0, 
         contadorantisimetrica = 0, 
-        M[MAXTAM][MAXTAM];
+        Matriz[MAXTAM][MAXTAM];
     char linha[MAXTAM];
     char reflexiva = 'F';
     char irreflexiva = 'F';
     char simetrica = 'F';
     char antisimetrica = 'V';
     char assimetrica = 'F';
-    char transitiva = 'F';
+    char transitiva = 'V';
     char relacaoequivalencia = 'F';
     char relacaoordempacial = 'F';
 
@@ -62,7 +62,7 @@ int main() {
 
     while(fgets(linha, 100, file) != NULL) {
         fscanf(file, "%i %i", &first, &second);
-        M[first][second] = 1;
+        Matriz[first][second] = 1;
     }
 
 	// printmatrix(M, n, m, name);
@@ -71,7 +71,7 @@ int main() {
     Tupla faltaReflexiva[n];
     int faltaReflexivaContagem = 0;
     for(i=name; i<n; i++) {
-        if(M[i][i] == 1) {
+        if(Matriz[i][i] == 1) {
             contadorref += 1;
         }
         else {
@@ -88,7 +88,7 @@ int main() {
     Tupla faltaIrreflexiva[n];
     int faltaIrreflexivaContagem = 0;
     for(i=name; i<n; i++) {
-        if(M[i][i] == 0) {
+        if(Matriz[i][i] == 0) {
             contadorirref += 1;
         }
         else {
@@ -106,12 +106,12 @@ int main() {
     int faltaSimetricaContagem = 0;
     for(i=name; i<n; i++) {
 		for(j=name; j<m; j++) {
-			if(M[i][j] == M[j][i]) {
+			if(Matriz[i][j] == Matriz[j][i]) {
                 contadorsimetrica += 1;
             }
-            else if((M[j][i] == 1) && (M[i][j] == 0)) {
+            else if((Matriz[j][i] == 1) && (Matriz[i][j] == 0)) {
                 faltaSimetrica[faltaSimetricaContagem].x = i;
-                faltaSimetrica[faltaSimetricaContagem].y = i;
+                faltaSimetrica[faltaSimetricaContagem].y = j;
                 faltaSimetricaContagem++;
             }
 		}
@@ -125,20 +125,17 @@ int main() {
     int faltaAntiSimetricaContagem = 0;
     for(i=name; i<n; i++) {
 		for(j=name; j<m; j++) {
-			if(M[i][j] == M[j][i]) {
-                if ((i!=j) && (M[i][j] == 1)){
+			if(Matriz[i][j] == Matriz[j][i]) {
+                if ((i!=j) && (Matriz[i][j] == 1)){
                     antisimetrica = 'F';
                     faltaAntiSimetrica[faltaAntiSimetricaContagem].x = i;
                     faltaAntiSimetrica[faltaAntiSimetricaContagem].y = j;
                     faltaAntiSimetricaContagem++;
-                    // printf("M[i][j]: %d\n", M[i][j]);
-                    // printf("M[j][i]: %d\n", M[j][i]);
-                    // printf("i, j: %i %i\n\n", i, j);
-                    // printf("contadorantisimetrica: %d\n", contadorantisimetrica);
                 }
             }
 		}
 	}
+
     //Verificação Assimétrica
     //É assimétrica se for irreflexiva e anti-simétrica
     if ((irreflexiva == 'V') && (antisimetrica == 'V')) {
@@ -146,19 +143,36 @@ int main() {
     }
 
     //Verificação Transitiva
+    Tupla faltaTransitiva[n];
+    int faltaTransitivaContagem = 0;
+    for (int i=name; i < n; i++){
+        for (int j=name; j < m; j++){
+            for (int k=name; k < n; k++){
+                if (Matriz[i][j] && Matriz[j][k] && !Matriz[i][k]){
+                    transitiva = 'F';
+                    faltaTransitiva[faltaTransitivaContagem].x = i;
+                    faltaTransitiva[faltaTransitivaContagem].y = k;
+                    faltaTransitivaContagem++;
+                    printf("Para ser Transitiva: (%d,%d);\n", j,i);
+                }
+            }
+        }
+    }
     
     //Relação de Equivalência
     if ((reflexiva == 'V') && (simetrica == 'V') && (transitiva == 'V')) {
         relacaoequivalencia = 'V';
     }
+
     //Relação de ordem parcial
     if ((reflexiva == 'V') && (transitiva == 'V') && (antisimetrica == 'V')) {
         relacaoordempacial = 'V';
     }
+
     // Verificação das relações encontradas
     // for(i=0; i<n; i++) {
 	// 	for(j=0; j<m; j++) {
-	// 		if(M[i][j] == 1){
+	// 		if(Matriz[i][j] == 1){
     //             printf("Tem relação: %i %i\n", i, j);
     //         }
 	// 	}
@@ -182,10 +196,13 @@ int main() {
     for (int i = 0; i < faltaAntiSimetricaContagem; i++){
         fprintf(saida, "(%d,%d); ", faltaAntiSimetrica[i].x, faltaAntiSimetrica[i].y);
     }
-    fprintf(saida, "\nAssimétrica: %c\n", assimetrica);
-    fprintf(saida, "\nTransitiva: to-do\n");
-    fprintf(saida, "\nRelação de equivalência: to-do");
-    fprintf(saida, "\nRelação de ordem parcial: to-do");
+    fprintf(saida, "\nAssimétrica: %c", assimetrica);
+    fprintf(saida, "\nTransitiva: %c\n", transitiva);
+    for (int i = 0; i < faltaTransitivaContagem; i++){
+        fprintf(saida, "(%d,%d); ", faltaTransitiva[i].x, faltaTransitiva[i].y);
+    }
+    fprintf(saida, "\nRelação de equivalência: %c", relacaoequivalencia);
+    fprintf(saida, "\nRelação de ordem parcial: %c", relacaoordempacial);
     fprintf(saida, "\nFecho transitivo da relação: to-do");
     
     fclose(file);
